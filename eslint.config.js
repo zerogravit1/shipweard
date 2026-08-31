5dbf385dbca0fb7d170a649d72d5644c07cbe952
Jon Schaffer <nograviti@gmail.com>
2026-08-31 00:57:04 -0400

    chore(config): finalize typecheck setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   js.configs.recommended,
   tseslint.configs.recommended,
-  eslintConfigPrettier.recommended
+  eslintConfigPrettier
 ]);
